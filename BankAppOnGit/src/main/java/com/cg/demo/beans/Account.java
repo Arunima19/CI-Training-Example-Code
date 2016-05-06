7557b53cb0564f419c0812de20bcb4b5862bff06
Arunima19 <arunima.banerjee2014@gmail.com>
2016-05-06 10:56:30 +0530

    in sonarqube

--- BankAppOnGit/src/main/java/com/cg/demo/beans/Account.java
+++ BankAppOnGit/src/main/java/com/cg/demo/beans/Account.java
@@ -42,15 +42,19 @@ public class Account {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj){
 			return true;
-		if (obj == null)
+			}
+		if (obj == null){
 			return false;
-		if (getClass() != obj.getClass())
+			}
+		if (getClass() != obj.getClass()){
 			return false;
+			}
 		Account other = (Account) obj;
-		if (accountNumber != other.accountNumber)
+		if (accountNumber != other.accountNumber){
 			return false;
+			}
 		return true;
 	}
 	
--- BankAppOnGit/src/main/java/com/cg/demo/util/AccountNumberGenerator.java
+++ BankAppOnGit/src/main/java/com/cg/demo/util/AccountNumberGenerator.java
@@ -2,6 +2,10 @@ package com.cg.demo.util;
 
 public class AccountNumberGenerator {
 	private static int accountNumber=1;
+	
+	private AccountNumberGenerator(){
+		
+	}
 	public static int getAccountNumber(){
 		return accountNumber++;
 	}
